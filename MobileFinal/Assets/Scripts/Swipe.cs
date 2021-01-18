72e13c4dbe15748f2a83e0057908d93b510f8325
Adrienmillotpro <63248529+Adrienmillotpro@users.noreply.github.com>
2021-01-18 14:01:56 +0100

    SwipeOpérationel
    :)

---
+++ MobileFinal/Assets/Scripts/Swipe.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swipe : MonoBehaviour
+{
+    public Transform ui;
+
+
+
+    void Update()
+    {
+        if (SwipeManager.swipeRight)
+        {
+            Debug.Log("Swipe Right");
+            ui.transform.position = ui.transform.position + new Vector3(20, 0, 0);
+
+        }
+        if (SwipeManager.swipeLeft)
+        {
+            Debug.Log("Swipe Left");
+            ui.transform.position = ui.transform.position + new Vector3(-20, 0, 0);
+        }
+        if (SwipeManager.swipeUp)
+        {
+            Debug.Log("Swipe Up");
+            ui.transform.position = ui.transform.position + new Vector3(0, 20, 0);
+        }
+        if (SwipeManager.swipeDown)
+        {
+            Debug.Log("Swipe Down");
+            ui.transform.position = ui.transform.position + new Vector3(0, -20, 0);
+
+
+        }
+
+
+    }
+}
