640d7dd71db8e5ef48a8b68e9563a237263a9fd9
Adrienmillotpro <63248529+Adrienmillotpro@users.noreply.github.com>
2021-01-18 21:23:21 +0100

    UI

--- MobileProject2021/Assets/Scripts/Swipe/Swipe.cs
+++ MobileProject2021/Assets/Scripts/Swipe/Swipe.cs
@@ -13,23 +13,23 @@ public class Swipe : MonoBehaviour
         if (SwipeManager.swipeRight)
         {
             Debug.Log("Swipe Right");
-            ui.transform.position = ui.transform.position + new Vector3(20, 0, 0);
+            ui.transform.position = ui.transform.position + new Vector3(100, 0, 0);
 
         }
         if (SwipeManager.swipeLeft)
         {
             Debug.Log("Swipe Left");
-            ui.transform.position = ui.transform.position + new Vector3(-20, 0, 0);
+            ui.transform.position = ui.transform.position + new Vector3(-100, 0, 0);
         }
         if (SwipeManager.swipeUp)
         {
             Debug.Log("Swipe Up");
-            ui.transform.position = ui.transform.position + new Vector3(0, 20, 0);
+            ui.transform.position = ui.transform.position + new Vector3(0, 100, 0);
         }
         if (SwipeManager.swipeDown)
         {
             Debug.Log("Swipe Down");
-            ui.transform.position = ui.transform.position + new Vector3(0, -20, 0);
+            ui.transform.position = ui.transform.position + new Vector3(0, -100, 0);
 
 
         }
