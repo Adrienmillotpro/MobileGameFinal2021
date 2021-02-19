ccdf108f603730e599f9ac77d09bbde036b1b3bc
Adrienmillotpro <63248529+Adrienmillotpro@users.noreply.github.com>
2021-02-19 18:04:17 +0100

    FX Heroes

--- MobileProject2021/Assets/Scripts/Visual/Hero_ParticuleSystem.cs
+++ MobileProject2021/Assets/Scripts/Visual/Hero_ParticuleSystem.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Hero_ParticuleSystem : MonoBehaviour
 {
-    [SerializeField] ParticleSystem particuleHit;
+    [SerializeField] ParticleSystem[] particuleHitsThunder = new ParticleSystem[3];
+    [SerializeField] ParticleSystem[] particuleHitsFire = new ParticleSystem[3];
+    [SerializeField] ParticleSystem[] particuleHitsAir = new ParticleSystem[3];
     [SerializeField] GameObject enemyPos;
 
 
@@ -24,16 +26,50 @@ public class Hero_ParticuleSystem : MonoBehaviour
         switch (damageArgs.bestHeroElement)
         {
             case ElementalTypes.Thunder:
-                if (damageArgs.bestElementalReaction == 2)
+              
+                if (damageArgs.bestElementalReaction == 0.5)
                 {
-                    Instantiate(particuleHit, enemyPos.transform);
+                    Instantiate(particuleHitsThunder[0], enemyPos.transform);
+                }
+                else if (damageArgs.bestElementalReaction == 1)
+                {
+                    Instantiate(particuleHitsThunder[1], enemyPos.transform);
+                }
+                else if (damageArgs.bestElementalReaction == 2)
+                {
+                    Instantiate(particuleHitsThunder[2], enemyPos.transform);
                 }
                 break;
             case ElementalTypes.Fire:
+                if (damageArgs.bestElementalReaction == 0.5)
+                {
+                    Instantiate(particuleHitsFire[0], enemyPos.transform);
+                }
+                else if (damageArgs.bestElementalReaction == 1)
+                {
+                    Instantiate(particuleHitsFire[1], enemyPos.transform);
+                }
+                else if (damageArgs.bestElementalReaction == 2)
+                {
+                    Instantiate(particuleHitsFire[2], enemyPos.transform);
+                }
                 break;
             case ElementalTypes.Water:
+
                 break;
             case ElementalTypes.Air:
+                if (damageArgs.bestElementalReaction == 0.5)
+                {
+                    Instantiate(particuleHitsAir[0], enemyPos.transform);
+                }
+                else if (damageArgs.bestElementalReaction == 1)
+                {
+                    Instantiate(particuleHitsAir[1], enemyPos.transform);
+                }
+                else if (damageArgs.bestElementalReaction == 2)
+                {
+                    Instantiate(particuleHitsAir[2], enemyPos.transform);
+                }
                 break;
         }
         
