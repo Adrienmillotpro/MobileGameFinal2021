b03fa779a96747d7f92eb1ef6dff5c8e8f6b056d
Adrienmillotpro <63248529+Adrienmillotpro@users.noreply.github.com>
2021-02-28 16:16:04 +0100

    Number Convertor

--- MobileProject2021/Assets/Scripts/UI/Currency/Ui_CurrencyElemental.cs
+++ MobileProject2021/Assets/Scripts/UI/Currency/Ui_CurrencyElemental.cs
@@ -8,6 +8,7 @@ using TMPro;
 public class Ui_CurrencyElemental : MonoBehaviour
 {
     [SerializeField] private TMP_Text text;
+    [SerializeField] private float temp;
 
     private void Awake()
     {
@@ -21,7 +22,25 @@ public class Ui_CurrencyElemental : MonoBehaviour
 
     private void OnUpdateCurrencyUpdateUI(OnUpdateCurrenciesEventArgs currencyArgs)
     {
-        text.text = currencyArgs.currentElemental.ToString("f0");
+        //text.text = currencyArgs.currentElemental.ToString("f0");
+
+        if (currencyArgs.currentElemental >= 1000000)
+        {
+            temp = currencyArgs.currentElemental / 1000000;
+            text.text = temp.ToString("F1") + "M";
+        }
+
+        else if (currencyArgs.currentElemental >= 1000)
+        {
+            temp = currencyArgs.currentElemental / 1000;
+            text.text = temp.ToString("F1") + "K";
+        }
+        else
+        {
+            temp = currencyArgs.currentElemental;
+            text.text = temp.ToString("F1");
+        }
+
     }
 }
 
