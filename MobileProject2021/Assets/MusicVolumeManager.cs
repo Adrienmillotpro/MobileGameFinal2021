fa4f715b2c554ba1d4f0aa274e76a3bde7712f8b
Adrienmillotpro <63248529+Adrienmillotpro@users.noreply.github.com>
2021-02-28 22:38:21 +0100

    Audio Manager

--- MobileProject2021/Assets/MusicVolumeManager.cs
+++ MobileProject2021/Assets/MusicVolumeManager.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Audio;
 
 public class MusicVolumeManager : MonoBehaviour
 {
     public AudioMixer mixer;
+    [SerializeField] private SO_MusicVolume musicVolume;
+    [SerializeField] private Slider sliderVolume;
+
+    private void Start()
+    {
+        sliderVolume.value = musicVolume.value;
+        mixer.SetFloat("MasterVolume", Mathf.Log10(musicVolume.value) * 20);
+    }
 
     public void VolumeLevel (float sliderValue)
     {
         mixer.SetFloat("MasterVolume", Mathf.Log10(sliderValue) * 20);
+        musicVolume.value = sliderValue;
     }
 
 
---
+++ MobileProject2021/Assets/Scripts/ScriptableObjects/Utilities/SO_MusicVolume.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SO_MusicVolume", menuName = "SO_MusicVolume")]
+
+public class SO_MusicVolume : ScriptableObject
+{
+    public float value;
+
+}
