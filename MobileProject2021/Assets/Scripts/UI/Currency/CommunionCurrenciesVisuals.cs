e8ea714c43b81acc75d4917ad952c4947014dbe3
SomeAvalancheGuy <59919173+SomeAvalancheGuy@users.noreply.github.com>
2021-03-01 10:13:35 +0100

    AKCHUALLLYYYYYYYYY

---
+++ MobileProject2021/Assets/Scripts/UI/Currency/CommunionCurrenciesVisuals.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CommunionCurrenciesVisuals : MonoBehaviour
+{
+    [SerializeField] SO_Player activePlayer;
+    [SerializeField] private TMP_Text textPremium;
+    [SerializeField] private TMP_Text textElemental;
+
+    private void Awake()
+    {
+        LevelUpGear.OnUpgradeGear += OnUpgradeGearUpdateCurrenciesUi;
+    }
+
+    private void OnDisable()
+    {
+        LevelUpGear.OnUpgradeGear -= OnUpgradeGearUpdateCurrenciesUi;
+    }
+
+    private void Start()
+    {
+        UpdatePreniumCurrencyUi();
+        UpdateElementalCurrencyUi();
+    }
+
+    private void OnUpgradeGearUpdateCurrenciesUi(OnUpdateGearEventArgs onUpdateGearArgs)
+    {
+        UpdatePreniumCurrencyUi();
+        UpdateElementalCurrencyUi();
+    }
+
+    private void UpdatePreniumCurrencyUi()
+    {
+        if (activePlayer.currencyPremium >= 1000000f)
+        {
+            textPremium.text = (activePlayer.currencyPremium / 1000000f).ToString("F1") + "M";
+        }
+        else if (activePlayer.currencyPremium >= 1000f)
+        {
+            textPremium.text = (activePlayer.currencyPremium / 1000f).ToString("F1") + "K";
+        }
+        else
+        {
+            textPremium.text = activePlayer.currencyPremium.ToString("F1");
+        }
+    }
+
+    private void UpdateElementalCurrencyUi()
+    {
+        if (activePlayer.currencyElemental >= 1000000f)
+        {
+            textElemental.text = (activePlayer.currencyElemental / 1000000f).ToString("F1") + "M";
+        }
+        else if (activePlayer.currencyElemental >= 1000f)
+        {
+            textElemental.text = (activePlayer.currencyElemental / 1000f).ToString("F1") + "K";
+        }
+        else
+        {
+            textElemental.text = activePlayer.currencyElemental.ToString("F1");
+        }
+
+
+    }
+}
